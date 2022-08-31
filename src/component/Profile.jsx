326f777c541c1e24fff1d9f827365682b88fc35d
hyelim3 <shl0703ss@gmail.com>
2022-08-31 18:04:57 +0900

    인스타 프로필 중후반

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Profile.css";
 
 const Profile = () => {
   return (
-    <div className="flex-col flex bg-red-500 h-128">
-      <div className="flex h-3/5 bg-yellow-500">
-        <div className="flex justify-center items-center w-1/3 bg-green-500">
+    <div className="flex-col flex  h-128">
+      <div className="flex h-3/5 ">
+        <div className="flex justify-center items-center w-1/3 ">
           <div className="avatar">
             <div className="w-40 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
               <img
@@ -14,7 +15,7 @@ const Profile = () => {
             </div>
           </div>
         </div>
-        <div className="w-2/3 bg-pink-500">
+        <div className="w-2/3 ">
           <div className="flex justify-end items-center h-2/5">
             <div className="text-2xl font-light mr-auto mt-2">SBS0712</div>
             <button className="rounded-md border-gray-400 bg-white text-black hover:bg-white text-black hover:rounded-md hover:border-gray-400 btn btn-sm mt-2 mr-4">
@@ -29,7 +30,7 @@ const Profile = () => {
           </div>
 
           <div className="h-1/5">
-            <div className="bg-yellow-500 h-16 flex items-center ">
+            <div className=" h-16 ">
               <a href="" className="mr-20">
                 게시물
               </a>
@@ -41,41 +42,55 @@ const Profile = () => {
               </a>
             </div>
           </div>
-          <div className="h-1/4">
-            <div className="bg-green-500 font-bold m-0 p-1">풀스택 A조</div>
-            <div className="bg-blue-500 p-1">instagram</div>
-            <a
-              className="font-bold text-blue-900 p-1"
-              href="https://github.com/hyelim3"
-            >
-              github.com/hyelim3
+          <div className="h-2/5">
+            <div className=" font-bold m-0 py-1">풀스택 A조</div>
+            <div className=" py-1">instagram</div>
+            <div className="font-bold text-blue-900 mt-1">
+              <a href="https://github.com/hyelim3">github.com/hyelim3</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-around items-center  h-1/5">
+        <div class="avatar ">
+          <div class=" relative w-20 rounded-full ring ring-offset-base-100 ring-offset-2">
+            <a href="">
+              <img src="https://i.picsum.photos/id/1038/3914/5863.jpg?hmac=SGtXryWDkn_eVQdA1NjYrikcsrIfcfS4SFYHo4lCpkQ" />
+            </a>
+          </div>
+        </div>
+        <div class="avatar">
+          <div class="w-20 rounded-full ring  ring-offset-base-100 ring-offset-2">
+            <a href="">
+              <img src="https://i.picsum.photos/id/1043/5184/3456.jpg?hmac=wsz2e0aFKEI0ij7mauIr2nFz2pzC8xNlgDHWHYi9qbc" />
+            </a>
+          </div>
+        </div>
+        <div class="avatar">
+          <div class=" w-20 rounded-full ring ring-offset-base-100 ring-offset-2">
+            <a href="">
+              <img src="https://i.picsum.photos/id/1060/5598/3732.jpg?hmac=31kU0jp5ejnPTdEt-8tAXU5sE-buU-y1W1qk_BsiUC8" />
+            </a>
+          </div>
+        </div>
+        <div class="avatar">
+          <div class=" w-20 rounded-full ring ring-offset-base-100 ring-offset-2">
+            <a href="">
+              <img src="https://i.picsum.photos/id/164/1200/800.jpg?hmac=wkqGUkaeW3kiAsHq_VwxSWWossIMAwFV4eUfFzuDkew" />
+            </a>
+          </div>
+        </div>
+        <div class="avatar">
+          <div class="  mr-16 w-20 rounded-full ring  ring-offset-base-100 ring-offset-2">
+            <a href="">
+              <img src="https://i.picsum.photos/id/120/4928/3264.jpg?hmac=i-8mkfKj_gRyQt9ZJVhbIBXbtIBNcsbI_gwNe_39vus" />
             </a>
           </div>
         </div>
       </div>
-
-      <div className="bg-blue-500 flex justify-around items-center h-2/5">
-        <div class="avatar ">
-          <div class=" mr-16 w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-            <img src="https://placeimg.com/192/192/people" />
-          </div>
-        </div>
-
-        <div class="avatar">
-          <div class="mr-16 w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-            <img src="https://placeimg.com/192/192/people" />
-          </div>
-        </div>
-        <div class="avatar">
-          <div class="mr-16 w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-            <img src="https://placeimg.com/192/192/people" />
-          </div>
-        </div>
-        <div class="avatar">
-          <div class="mr-16 w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-            <img src="https://placeimg.com/192/192/people" />
-          </div>
-        </div>
+      <div class="tabs flex justify-center mt-9">
+        <a class="tab tab-bordered ">게시글</a>
+        <a class="tab tab-bordered tab-active">태그 됨</a>
       </div>
     </div>
   );
